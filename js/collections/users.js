af7d553535d8e3fe0660ecde8905341fa5d40754
Romualdo Dasig <dasig.rg@a5project.com>
2014-10-01 07:59:33 +0800

    Update user

--- js/collections/users.js
+++ js/collections/users.js
@@ -2,15 +2,15 @@ define([
     'backbone',
     'models/user'
 ], function (Backbone, UserModel) {
-    
+
     $.ajaxPrefilter( function(options, originalOptions, jqXHr) {
         options.url = 'http://api.laravelcommerce.local/v1' + options.url;
     });
-    
+
     var UserCollection = Backbone.Collection.extend({
-        url: '/user',
+        url: '/users',
         model: UserModel
     });
-    
+
     return UserCollection;
 });
